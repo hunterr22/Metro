f9907e4068f2efc0d7eb261f76a37a89eb53f9dd
hunterr22 <99999248+hunterr22@users.noreply.github.com>
2022-10-15 09:08:50 -0400

    Update CustomerAPI.java

--- metro/src/main/java/com/metro/api/CustomerAPI.java
+++ metro/src/main/java/com/metro/api/CustomerAPI.java
@@ -1,19 +1,13 @@
 package com.metro.api;
 
-import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.metro.domain.Customer;
 import com.metro.repository.CustomersRepository;
